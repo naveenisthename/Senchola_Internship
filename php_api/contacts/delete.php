80d9824f485031b54a8b55f45c6e450e9824d287
Naveenkumar <108060961+naveenisthename@users.noreply.github.com>
2023-10-12 17:19:23 +0530

    Add files via upload

---
+++ php_api/contacts/delete.php
@@ -0,0 +1,22 @@
+<?php
+
+header('Access-Control-Allow-Origin:*');
+header('Content-Type:application/json');
+header('Access-Control-Allow-Methods:DELETE');
+header('Access-Control-Allow-Headers:Content-type,Access-Control-Allow-Headers,Authorization,x-Request-with');
+
+include('funtion.php');
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+if ($requestMethod == "DELETE") {
+    $deleteContact = deleteContact($_GET);
+    echo $deleteContact;
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
